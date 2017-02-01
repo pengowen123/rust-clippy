1b7a6f73b995c5d1b3c9efebe5071cb5b6641b61
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-02-01 09:21:14 +0100

    update help message to match the suggestion

--- tests/compile-fail/for_loop.rs
+++ tests/compile-fail/for_loop.rs
@@ -288,24 +288,24 @@ fn main() {
 
     for _v in vec.iter() { }
     //~^ ERROR it is more idiomatic to loop over references to containers instead of using explicit iteration methods
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &vec {
 
     for _v in vec.iter_mut() { }
     //~^ ERROR it is more idiomatic to loop over references to containers instead of using explicit iteration methods
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &mut vec {
 
     let out_vec = vec![1,2,3];
     for _v in out_vec.into_iter() { }
     //~^ ERROR it is more idiomatic to loop over containers instead of using explicit iteration methods
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in out_vec {
 
     let array = [1, 2, 3];
     for _v in array.into_iter() {}
     //~^ ERROR it is more idiomatic to loop over references to containers instead of using explicit iteration methods
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &array {
 
     for _v in &vec { } // these are fine
@@ -313,14 +313,14 @@ fn main() {
 
     for _v in [1, 2, 3].iter() { }
     //~^ ERROR it is more idiomatic to loop over references to containers
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &[1, 2, 3] {
 
     for _v in (&mut [1, 2, 3]).iter() { } // no error
 
     for _v in [0; 32].iter() {}
     //~^ ERROR it is more idiomatic to loop over references to containers
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &[0; 32] {
 
     for _v in [0; 33].iter() {} // no error
@@ -328,43 +328,43 @@ fn main() {
     let ll: LinkedList<()> = LinkedList::new();
     for _v in ll.iter() { }
     //~^ ERROR it is more idiomatic to loop over references to containers
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &ll {
 
     let vd: VecDeque<()> = VecDeque::new();
     for _v in vd.iter() { }
     //~^ ERROR it is more idiomatic to loop over references to containers
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &vd {
 
     let bh: BinaryHeap<()> = BinaryHeap::new();
     for _v in bh.iter() { }
     //~^ ERROR it is more idiomatic to loop over references to containers
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &bh {
 
     let hm: HashMap<(), ()> = HashMap::new();
     for _v in hm.iter() { }
     //~^ ERROR it is more idiomatic to loop over references to containers
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &hm {
 
     let bt: BTreeMap<(), ()> = BTreeMap::new();
     for _v in bt.iter() { }
     //~^ ERROR it is more idiomatic to loop over references to containers
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &bt {
 
     let hs: HashSet<()> = HashSet::new();
     for _v in hs.iter() { }
     //~^ ERROR it is more idiomatic to loop over references to containers
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &hs {
 
     let bs: BTreeSet<()> = BTreeSet::new();
     for _v in bs.iter() { }
     //~^ ERROR it is more idiomatic to loop over references to containers
-    //~| HELP to write this more concisely, try looping over
+    //~| HELP to write this more concisely, try
     //~| SUGGESTION for _v in &bs {
 
 
